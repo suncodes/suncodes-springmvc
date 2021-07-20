35c6b9474f0497ee826e3eb97d434d324596c5c3
sunchuizhe <2605587563@qq.com>
2021-07-20 23:14:46 +0800

    request

---
+++ suncodes-springmvc-request/src/main/java/controller/RegisterController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class RegisterController {
+
+    @RequestMapping("/register")
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
+            throws Exception {
+        return new ModelAndView("/login");
+    }
+}
