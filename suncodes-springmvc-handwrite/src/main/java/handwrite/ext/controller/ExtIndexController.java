da6f8e9bdb1790e7776bf2c4b04860e7009bc21c
sunchuizhe <2605587563@qq.com>
2021-07-29 23:10:07 +0800

    手写springmvc框架

---
+++ suncodes-springmvc-handwrite/src/main/java/handwrite/ext/controller/ExtIndexController.java
@@ -0,0 +1,17 @@
+package handwrite.ext.controller;
+
+import handwrite.extspringmvc.extannotation.ExtController;
+import handwrite.extspringmvc.extannotation.ExtRequestMapping;
+
+@ExtController
+@ExtRequestMapping(value = "/ext")
+public class ExtIndexController {
+
+	// url地址如何 和方法进行关联
+	@ExtRequestMapping(value = "/index")
+	public String index() {
+		System.out.println("手写springmvc框架");
+		return "index";
+	}
+
+}
